fee47d5817f026e8b959129f8f871cac6492a047
Naomi <naomilang1212@gmail.com>
2021-04-01 19:33:45 -0700

    switching landing to react

---
+++ client/src/components/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Landing() {
+    return (
+    <section>
+            <header>
+                <img class="img" src="./assets/MyLitics.gif" width="100%"/>
+            </header>
+            <div>
+                    <img class="img" src="./assets/login.png" width="20%"/>
+
+            </div>
+                <div>
+                    <img class="img" src="./assets/signup.png" width="20%"/>
+    </div>
+    </section>
+    );
+}
+
+export default Landing;
