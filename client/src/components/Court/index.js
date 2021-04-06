d12da1e79480a4bfe65ab8d8026bcd64c8a52eca
XXLZopes <afordm99@gmail.com>
2021-04-06 00:13:40 -0700

    finished court and enabled dynamic colors

--- client/src/components/Court/index.js
+++ client/src/components/Court/index.js
@@ -1,8 +1,10 @@
 import React, {useRef, useEffect, useState} from 'react';
 let pinnedLocationCount = 0;
+let courtLineColor = 'teal';
+let paintColor = 'lime';
+let playerInnerColor = 'white';
 function Court() {
-
-    let canvasWidth = window.innerWidth <= 475 ? window.innerWidth : 475;
+    let canvasWidth = window.innerWidth <= 500 ? window.innerWidth : 500;
     const canvasRef = useRef(null);
 
     //get mouse position custom hook
@@ -31,10 +33,11 @@ function Court() {
         const canvas = canvasRef.current
         const ctx = canvas.getContext('2d')
 
-        ctx.fillStyle = 'red'
+        ctx.fillStyle = playerInnerColor;
+
         ctx.beginPath()
         xPos && yPos ? ctx.arc(xPos, yPos, 10, 0, 2*Math.PI) : ctx.arc(xPos, yPos, 0, 0, 2*Math.PI)
-        ctx.fill()
+        ctx.fill();
 
         console.log('x: ',xPos,'y: ',yPos);
     }, [pinnedLocationCount]);
@@ -44,17 +47,13 @@ function Court() {
         const canvas = canvasRef.current
         const ctx = canvas.getContext('2d')
         let width = canvas.width;
-        let height = window.innerHeight;
-        let courtLinesWidth = width*height*.000012;
+        let height = canvas.height;
+        let courtLinesWidth = width*height*.000013;
         let center = width/2;
         let half = height/2;
-        ctx.fillStyle = 'white'
-        ctx.strokeStyle = 'white'
+        ctx.strokeStyle = courtLineColor
+
         ctx.lineWidth = (courtLinesWidth)
-
-        function drawOutOfBounds() {
-
-        }
 
         //Draw Half Court
         function drawHalf() {
@@ -62,6 +61,7 @@ function Court() {
             let halfCourtY = height/2
             let bigCircleWidth = width*.15
             let smallCircleWidth = width *.05
+            ctx.fillStyle = courtLineColor;
 
             //horizontal line
             ctx.beginPath()
@@ -69,9 +69,9 @@ function Court() {
             ctx.lineTo(width, halfCourtY);
             ctx.stroke();
             //circle small
-            ctx.arc(halfCourtX, halfCourtY, smallCircleWidth, 0, 2*Math.PI);
+            ctx.arc(center, halfCourtY, smallCircleWidth, 0, 2*Math.PI);
             //circle big
-            ctx.arc(halfCourtX, halfCourtY, bigCircleWidth, 0, 2*Math.PI);
+            ctx.arc(center, halfCourtY, bigCircleWidth, 0, 2*Math.PI);
             ctx.stroke();
             return(
                 {
@@ -89,14 +89,16 @@ function Court() {
                     //center+with & center-width = x values for 2 lines
                 
                 let y;
-                let distanceFromHalf = half-half/2-drawHalf().bigCircleWidth
+                let distanceFromHalf = height/7
                 let r = width/2.5
-                    for(let x=0; x<=width; x+=.5) {
+                let distanceBetweenLines = center*2
+                    for(let x=0; x<=width/2; x+=.5) {
                       //Draw half circle
                     ctx.beginPath()
-                    let y= .5*Math.sqrt((r*r)-x*x)+distanceFromHalf
-                    ctx.arc(x+center, y, courtLinesWidth/2.2, 0, 2*Math.PI);
-                    ctx.arc(-x+center, y, courtLinesWidth/2.2, 0, 2*Math.PI);
+                    console.log(distanceBetweenLines)
+                    let y= .85*Math.sqrt((r*r)-x*x)+distanceFromHalf
+                    ctx.arc(x+center, y-50, courtLinesWidth/2.2, 0, 2*Math.PI);
+                    ctx.arc(-x+center, y-50, courtLinesWidth/2.2, 0, 2*Math.PI);
                     ctx.fill();
                 
                     }
@@ -108,14 +110,15 @@ function Court() {
 
                     ctx.beginPath()
                     ctx.moveTo(rx, 0);
-                    ctx.lineTo(rx, y+distanceFromHalf);
+                    ctx.lineTo(rx, y+distanceFromHalf-35);
                     ctx.stroke();
 
                     let lx = center-width/2.5
                     ctx.beginPath()
                     ctx.moveTo(lx, 0);
-                    ctx.lineTo(lx, y+distanceFromHalf);
+                    ctx.lineTo(lx, y+distanceFromHalf-35);
                     ctx.stroke();
+                    
 
                     return({
                         rightX: rx,
@@ -128,13 +131,14 @@ function Court() {
 
         //Draw Free Throw
             function drawFreeThrow() {
+                
             let height = drawThreePointLine().height;
-
+            
             //Start at 
                 let y;
                 let r = width/7
-                for(let x=0; x<=width; x+=.5) {
-                    //Draw half circle
+                for(let x=0; x<=width/2; x+=.5) {
+               //Draw half circle
                   ctx.beginPath()
                   y= Math.sqrt((r*r)-x*x)+height*.93
                   ctx.arc(x+center, y, courtLinesWidth/2.2, 0, 2*Math.PI);
@@ -142,40 +146,117 @@ function Court() {
                   ctx.fill();
               
                   }
-
-                  let x = center+r
+                  //right
+                    let x = center+r
+                    let xOne = x
                     y= .02655*Math.sqrt(r*r)*2
                     console.log('x: ',x)
                     console.log('y: ', y)
 
-                    ctx.beginPath()
-                    ctx.moveTo(x, 0);
-                    ctx.lineTo(x, height);
-                    ctx.stroke();
+                 
 
+                   
+                    //left
                     x = center-r
-                    ctx.beginPath()
+                    let distance = xOne - x+20;
+                    
+                    //draw paint
+                    ctx.beginPath();
+                    ctx.rect(x-10, 0, distance, height)
+                    ctx.strokeStyle = courtLineColor;
+                    ctx.fillStyle = paintColor;
+                    ctx.fill();
+                    ctx.stroke();
+
+                    //left small horizontal lines
+                    //1
+                    ctx.beginPath();
+                    ctx.moveTo(x-10,height-20);
+                    ctx.lineTo(x-20, height-20);
+                    ctx.stroke();
+                    //2
+                    ctx.beginPath();
+                    ctx.moveTo(x-10,height-40);
+                    ctx.lineTo(x-20, height-40);
+                    ctx.stroke();
+                    //3
+                    ctx.beginPath();
+                    ctx.moveTo(x-10,height-60);
+                    ctx.lineTo(x-20, height-60);
+                    ctx.stroke();
+                    //4
+                    ctx.beginPath();
+                    ctx.moveTo(x-10,height-70);
+                    ctx.lineTo(x-20, height-70);
+                    ctx.stroke();
+
+                    //-1
+                    ctx.beginPath();
+                    ctx.moveTo(xOne+10,height-20);
+                    ctx.lineTo(xOne+20, height-20);
+                    ctx.stroke();
+                    //-2
+                    ctx.beginPath();
+                    ctx.moveTo(xOne+10,height-40);
+                    ctx.lineTo(xOne+20, height-40);
+                    ctx.stroke();
+                    //-3
+                    ctx.beginPath();
+                    ctx.moveTo(xOne+10,height-60);
+                    ctx.lineTo(xOne+20, height-60);
+                    ctx.stroke();
+                    //-4
+                    ctx.beginPath();
+                    ctx.moveTo(xOne+10,height-70);
+                    ctx.lineTo(xOne+20, height-70);
+                    ctx.stroke();
+
+                    //draw left line
+                    ctx.beginPath();
                     ctx.moveTo(x, 0);
                     ctx.lineTo(x, height);
                     ctx.stroke();
 
-                
-                
+                    //draw right line
+                    x = center+r
+                    ctx.beginPath()
+                    ctx.moveTo(x, 0);
+                    ctx.lineTo(x, height);
+                    ctx.stroke();
+                    
+
+                    for(let x=0; x<=width/2; x+=.5) {
+                        //Draw half circle
+                           ctx.fillStyle = courtLineColor;
+                           ctx.beginPath()
+                           y= -Math.sqrt((r*r)-x*x)+height*.93
+                           ctx.arc(x+center, y+5, courtLinesWidth/2.2, 0, 2*Math.PI);
+                           ctx.arc(-x+center, y+5, courtLinesWidth/2.2, 0, 2*Math.PI);
+                           ctx.fill();
+                       
+                           }
             } 
             drawFreeThrow()
+
+            function drawOutOfBounds() {
+                ctx.lineWidth = courtLinesWidth*3
+                ctx.beginPath();
+                
+                ctx.rect(0,0,width,height)
+                ctx.stroke();
+                
+            } drawOutOfBounds()
 
 
             
             console.log('width: ', width, 'height: ', height)
     }, []) //Add use effect when screen size variables change [screen size variable]
     
-    let canvasX
-    window.innerWidth<650 ? canvasX = window.innerWidth : canvasX = 650
     return(
         <canvas 
         ref={canvasRef} 
         width={canvasWidth} 
-        height={window.innerHeight}
+        height={window.innerHeight-canvasWidth/5}
         onClick={() => {
             //change pinnedLocationCount to be an array of objects that gets set to ...pinnedLocation + {new object info}
             pinnedLocationCount++
